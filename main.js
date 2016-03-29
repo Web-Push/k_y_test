675cdfd82178eebc6379dbb11737a2cb7b491dd2
yt-harada <yt-harada@kddi-tech.com>
2016-03-29 10:35:07 +0900

    誤配信チェック対応（コードは微妙だけど）

--- main.js
+++ main.js
@@ -211,13 +211,19 @@ function sendSubscriptionToServer(subscription) {
   userId = readCookie();
   endpoint = endpointWorkaround(subscription);
   serviceUrl = window.location.href;
-  
+
   console.log('userId :' + userId);
   console.log('endpoint :' + endpoint);
   console.log('serviceUrl :' + serviceUrl);
-  
+
   registerData(userId, endpoint, serviceUrl);
 
+
+  navigator.serviceWorker.controller.postMessage({
+    action: 'LoginUser',
+    userId: userId,
+    serviceUrl: serviceUrl
+  });
 }
 
 
--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,32 @@
+/** Webサイト側からPostMessageで送られたデータを受信する */
+self.addEventListener('message', function (event) {
+  console.log(event.data.action);
+  console.log(event.data.userId);
+  console.log(event.data.serviceUrl);
+  
+  writeDB(event.data.userId, event.data.serviceUrl);
+});
+
+/** Push通知を受けたときの処理 */
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
 
-  var title = 'k_y_test.';
-  var body = 'k_y_test のページで登録したServiceWorkerです。';
-  var icon = '/images/icon-192x192.png';
-  var tag = 'simple-push-demo-notification-tag';
-
   event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
+    fetch('https://web-push.github.io/WebPushControl/users.json').then(function(response){
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ', response.status);
+      } else {
+        response.text().then(function(textdata) {
+          console.log('text:', textdata);
+          var jsondata = JSON.parse(textdata);
+          checkLogin(jsondata);
+        });
+      }
     })
   );
 });
 
+/** Notificationをクリックしたときの処理 */
 self.addEventListener('notificationclick', function(event) {
   console.log('On notification click: ', event.notification.tag);
   // Android doesn’t close the notification when you click on it
@@ -38,7 +50,119 @@ self.addEventListener('notificationclick', function(event) {
   }));
 });
 
+/** ServiceWorkerのactivateが完了したときの処理 */
 self.addEventListener('activate', function(event) {
+  createDB();
   event.waitUntil(self.clients.claim());
   console.log('activate Complete');
 });
+
+
+/** indexDBの生成処理 */
+function createDB(){
+  // chrome以外のブラウザで試すときは[var database = indexedDB]の定義を変えないと多分動かない
+  var database = indexedDB;
+  var db = null;
+
+  if (database) {
+    database.deleteDatabase("mydb");
+    var openRequest = database.open("mydb", 1.0);
+    openRequest.onupgradeneeded = function(event) {
+      // データベースのバージョンに変更があった場合(初めての場合もここを通ります。)
+      db = event.target.result;
+      var store = db.createObjectStore("books", { keyPath: "mykey"});
+      // インデックスを作成します。
+      store.createIndex("myvalueIndex", "myvalue");
+    }
+
+    openRequest.onsuccess = function(event) {
+      db = event.target.result;
+    }
+  }
+}
+
+/** indexDBへ書き込み処理（書き方か読み方を変えないと面倒） */
+function writeDB(user, url){
+  var database = indexedDB;
+  var req = database.open("mydb");
+  var db = null;
+
+  //成功時コールバック
+  req.onsuccess = function(evt) {
+    db = evt.target.result;
+    var transaction = db.transaction(["books"], "readwrite");
+    var store = transaction.objectStore("books");
+
+    //データ追加
+    store.put({ mykey: "user", myvalue: user});
+    store.put({ mykey: "url", myvalue: url});
+  }
+}
+
+/** indexDBに登録されているデータとfetchしてきたデータとのマッチング */
+function checkLogin(jsondata) {
+  var database = indexedDB;
+  var req = database.open("mydb");
+  var db = null;
+  var user = null;
+  var url = null;
+
+  //成功時コールバック
+  req.onsuccess = function(evt) {
+    db = evt.target.result;
+    var transaction = db.transaction(["books"], "readwrite");
+    var store = transaction.objectStore("books");
+    var request = store.get("user");
+    request.onsuccess = function(evt) {
+      if (evt.target.result === undefined) {
+        console.log('キーが存在しない');
+      } else {
+        // 取得成功
+        console.log(evt.target.result.myvalue);
+        user = evt.target.result.myvalue;
+      }
+    };
+
+    var request2 = store.get("url");
+    request2.onsuccess = function(evt) {
+      var result = false;
+      if (evt.target.result === undefined) {
+        console.log('キーが存在しない');
+      } else {
+        // 取得成功
+        console.log(evt.target.result.myvalue);
+        url = evt.target.result.myvalue;
+        var cnt = 0;
+        while (jsondata.users.length > cnt) {
+          console.log('user_id:', jsondata.users[cnt].user_id);
+          console.log('service_url:', jsondata.users[cnt].service_url);
+          if (user === jsondata.users[cnt].user_id && url === jsondata.users[cnt].service_url) {
+            result = true;
+          }
+          cnt++;
+        }
+      }
+      showNotification(result);
+    };
+  };
+}
+
+/** Notificationの表示処理 */
+function showNotification(result) {
+  var title = 'yt-test.';
+  var body = '';
+  var icon = '/images/icon-192x192.png';
+  var tag = 'simple-push-demo-notification-tag';
+
+  if (result === true) {
+    body = 'ログイン状態です';
+  } else {
+    body = 'ログアウト状態です';
+  }
+
+  self.registration.showNotification(title, {
+    body: body,
+    icon: icon,
+    tag: tag
+  });
+}
